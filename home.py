1311861e4268ba41e7a5037aa598e29107faa27b
Long <lytlong.pers@gmail.com>
2024-10-23 13:52:50 +0700

    downgrade streamlit for wallpaper

--- home.py
+++ home.py
@@ -3,8 +3,26 @@ import requests
 from PIL import Image
 from io import BytesIO
 import os
-
+import base64
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+# set_background
+def set_background():
+    page_bg_img = f"""
+    <style>
+    [data-testid="stAppViewContainer"] {{
+        background-image: url("/static/Background.png");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }}
+    </style>
+    """
+    st.markdown(page_bg_img, unsafe_allow_html=True)
 def display_home(): # Task for Nguyen Dang Minh, Ninh Duy Tuan
+    set_background()
     st.balloons()
 
     # Your function goes here!
--- main.py
+++ main.py
@@ -3,18 +3,16 @@ from st_pages import add_page_title, get_nav_from_toml
 import base64
 import webbrowser
 st.set_page_config(layout = "wide")
-@st.cache_data 
+@st.cache_data
 
 # import css
 def load_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-# import image
 def get_img_as_base64(file):
     with open(file, "rb") as f:
         data = f.read()
     return base64.b64encode(data).decode()
-# set_background
 def set_background():
     img = get_img_as_base64("Background.png")
     page_bg_img = f"""
@@ -26,15 +24,14 @@ def set_background():
         background-attachment: local;
         }}
         </style>
-        """     
+        """ 
     st.markdown(page_bg_img, unsafe_allow_html=True)
-
 
 # main function
 def main():
     set_background()
     nav = get_nav_from_toml("pages.toml")
-    pg = st.navigation(nav, expanded=False)
+    pg = st.navigation(nav)
     add_page_title(pg)
     pg.run()
     
