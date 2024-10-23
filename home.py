0dec9db2b35d615355f6eb9860375268983abb19
Long <lytlong.pers@gmail.com>
2024-10-23 14:23:24 +0700

    update

--- home.py
+++ home.py
@@ -1,25 +1,23 @@
 import streamlit as st
 import requests
-from PIL import Image
-from io import BytesIO
-import os
 import base64
 def get_img_as_base64(file):
     with open(file, "rb") as f:
         data = f.read()
     return base64.b64encode(data).decode()
 # set_background
+img = get_img_as_base64("Background.png")
 def set_background():
     page_bg_img = f"""
-    <style>
-    [data-testid="stAppViewContainer"] {{
-        background-image: url("/static/Background.png");
-        background-size: cover;
+        <style>
+        [data-testid="stAppViewContainer"] > .main {{
+        background-image: url("data:image/png;base64,{img}");
+        background-size: 100%;
         background-repeat: no-repeat;
-        background-attachment: fixed;
-    }}
-    </style>
-    """
+        background-attachment: local;
+        }}
+        </style>
+        """ 
     st.markdown(page_bg_img, unsafe_allow_html=True)
 def display_home(): # Task for Nguyen Dang Minh, Ninh Duy Tuan
     set_background()
--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import streamlit as st
 from st_pages import add_page_title, get_nav_from_toml
 import base64
-import webbrowser
 st.set_page_config(layout = "wide")
 @st.cache_data
-
 
 # import css
 def load_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-
 # main function
 def main():
-    set_background()
     nav = get_nav_from_toml("pages.toml")
     pg = st.navigation(nav)
     add_page_title(pg)
