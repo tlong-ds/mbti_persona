4f27ede482f4526f61439988e63cff3e983f40ba
Long <lytlong.pers@gmail.com>
2024-10-27 11:01:48 +0700

    update

--- clear.sh
+++ clear.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+git checkout main
 git branch | grep -v "main" | xargs git branch -d
 
 git pull upstream main
--- run.sh
+++ run.sh
@@ -8,8 +8,13 @@ git push origin main
 
 # create branch if branch name was passed
 if [ ! -z "$branch" ]; then
-    git checkout -b "$branch"
-    echo "You are working on: $branch"
+    if git show-ref --verify --quiet "refs/heads/$branch"; then
+        git checkout "$branch"
+        echo "You are working on: $branch"
+    else
+        git checkout -b "$branch"
+        echo "$branch has been created! You are working on: $branch"
+    fi  
     git branch
 fi
 # run the project
