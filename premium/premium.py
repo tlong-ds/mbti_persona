03fdbf83e7e558c7432f2e35a3e64b8a62ecb029
Long <lytlong.pers@gmail.com>
2024-10-28 20:40:18 +0700

    update

---
+++ premium/premium.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import base64
+
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+# set_background
+img = get_img_as_base64("./premium/Background.png")
+def set_background():
+    page_bg_img = f"""
+        <style>
+        [data-testid="stAppViewContainer"] > .main {{
+        background-image: url("data:image/png;base64,{img}");
+        background-size: 100%;
+        background-repeat: no-repeat;
+        background-attachment: local;
+        }}
+        </style>
+        """ 
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+def display_premium(): # Task for Truong Duc Anh
+    set_background()
+    st.markdown("")
+
+display_premium()
