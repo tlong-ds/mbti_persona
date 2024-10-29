23faef596461f86255f30207785638474257df25
Long <lytlong.pers@gmail.com>
2024-10-29 09:43:20 +0700

    update

--- clear.sh
+++ clear.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+branch="$1"
+if [ -z "$branch" ]; then
+    echo "Usage: ./clear.sh <your_branch>"
+# stage changes
+fi
 git checkout main
-git branch | grep -v "main" | xargs git branch -d
+git branch -D "$branch"
 
 git pull upstream main
 git push origin main
