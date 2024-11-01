4efce712293ce324fd218da06ae27964b853023a
Long <lytlong.pers@gmail.com>
2024-11-01 13:14:40 +0700

    update

--- about/about.py
+++ about/about.py
@@ -18,7 +18,7 @@ def display_about():
     col1, col2 = st.columns([1, 3])
     
     with col1:
-        st.image("./about/logo.png", width=200) 
+        st.image("./about/logo.webp", width=200) 
     with col2:
         st.header("Our Offerings")
         
